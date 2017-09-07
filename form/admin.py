19a3d37dd001d64090b4a49c5f0f2906b39383ec
Devkota <info@devkotasagar.com.np>
2017-09-07 07:54:56 +0545

    Register models on admin

--- form/admin.py
+++ form/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Programming_Competition,NOSKode
+
+class Programming_CompetitionAdmin(admin.ModelAdmin):
+    list_display=("id","full_name", "email","phone_no","programming_language")
+    
+class NOSKodeAdmin(admin.ModelAdmin):
+    list_display=("id","team_name","team_leader","email","phone_number","total_team_members","project_title","project_description")
+
+
+admin.site.register(Programming_Competition, Programming_CompetitionAdmin)
+admin.site.register(NOSKode, NOSKodeAdmin)
